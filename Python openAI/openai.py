a685990b0fd9744166d8f852ae9be469850f2720
Toufickh <108784777+Toufickh@users.noreply.github.com>
2024-06-08 18:49:30 +0200

    Update openai.py

--- Python openAI/openai.py
+++ Python openAI/openai.py
@@ -1,6 +1,7 @@
 print("Please select one of the following formulas to do: \n")
 print("1: Add two numbers \n")
-print("2: odd or even numbers \n")
+print("2: Check if a number is odd or even \n")
+print("3: Reverse a number \n")
 vari = int (input("Value: "))
 print("\n")
 
@@ -15,5 +16,14 @@ elif vari == 2:
     print("Number is Even")
   else:
     print("Number is Odd")
+elif vari == 3:
+  bromo = int(input("Please input the number to rotate: "))
+  baram = 0
+  while (bromo > 0):
+    remainder = bromo % 10
+    baram = (baram * 10) + remainder
+    bromo = bromo // 10
+  print("The reversed number is:", baram)
+  print("\n")
 else:
   print("This input is not acceptable \n")
